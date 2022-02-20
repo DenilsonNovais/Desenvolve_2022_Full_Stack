241a6e6b32f3234239eadd0c0d47fe85b5613965
Denilson Santos <denilson_n.santos@hotmail.com>
2022-02-20 16:11:12 -0300

    trabalhando com tipos boleanos

---
+++ SEMANA_07/Fundamentos do JavaScript - Tipos, variáveis e funções/type-boolean.js
@@ -0,0 +1,19 @@
+/* Boolean
+    true => verdadeiro
+    false => falso
+*/
+
+const primeiroNumero = 5;
+const segundoNumero = "5";
+let cadastroAtivo = true;
+
+// Não compara o tipo da variavel, só o seu valor
+console.log(primeiroNumero == segundoNumero);
+
+// Compara tudo até o tipo da variavel
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
