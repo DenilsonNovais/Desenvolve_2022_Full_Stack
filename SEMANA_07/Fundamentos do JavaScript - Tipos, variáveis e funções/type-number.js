027c91271d3fad96edf16afee1e952a6e7ffb935
Denilson Santos <denilson_n.santos@hotmail.com>
2022-02-20 14:20:00 -0300

    Trabalhando com tipos numericos em javaScript
    Inicio do curso Fundamentos do JavaScript: Tipos, variáveis e funções

---
+++ SEMANA_07/Fundamentos do JavaScript - Tipos, variáveis e funções/type-number.js
@@ -0,0 +1,38 @@
+// Tipo number 
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacao);
+
+// NaN => Not a Number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero); // NaN
+
+// Exemplos
+
+const idade = 28
+const pi = 3.14
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
