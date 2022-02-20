2da10b24bbb83c727afbd4de1cfe51d811a14289
Denilson Santos <denilson_n.santos@hotmail.com>
2022-02-20 15:44:16 -0300

    Trabalhando com tipos strings

---
+++ SEMANA_07/Fundamentos do JavaScript - Tipos, variáveis e funções/type-string.js
@@ -0,0 +1,40 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "SenhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Denilson";
+
+// Concatenação (+)
+
+console.log(citacao + meuNome);
+
+// Template string ou Templa literal
+
+
+// Exemplo de caractere UniCode
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const minhaSenha = "minhaSenha123";
+console.log(minhaSenha.length); // 13 caracteres
+
+
