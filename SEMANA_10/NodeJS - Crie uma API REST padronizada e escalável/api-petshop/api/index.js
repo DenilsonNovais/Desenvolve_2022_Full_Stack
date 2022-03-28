6e167063229df42d0617b37b84ee25616e7db199
Denilson Santos <denilson_n.santos@hotmail.com>
2022-03-27 23:45:03 -0300

    Usando middlewares em uma api express

--- SEMANA_10/NodeJS - Crie uma API REST padronizada e escalável/api-petshop/api/index.js
+++ SEMANA_10/NodeJS - Crie uma API REST padronizada e escalável/api-petshop/api/index.js
@@ -2,11 +2,39 @@ const express = require('express');// Utilizamos o express para cria a API
 const app = express();//  Instancia do express para gera uma aplicação
 const bodyParser = require('body-parser');// Utiliza o body-parser para declaraar o formato a utilizar
 const config = require('config');// Chama as configurações do banco e passa uma nova porta para o listen
+const NaoEncontrado = require('./erros/NaoEncontrado')
+const CampoInvalido = require('./erros/CampoInvalido')
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 app.use(bodyParser.json());// Declara o json como formato a ser utilizado pela api
 
 const roteador = require('./rotas/fornecedores');
 app.use('/api/fornecedores', roteador);// consumindo api de rotas dentro da api principal
+
+app.use((erro, requisicao, resposta, proximo) => {
+    let status = 500
+
+    if (erro instanceof NaoEncontrado) {
+        status = 404
+    }
+
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
+        status = 400
+    }
+
+    if (erro instanceof ValorNaoSuportado) {
+        status = 406
+    }
+
+    resposta.status(status)
+    resposta.send(
+        JSON.stringify({
+            mensagem: erro.message,
+            id: erro.idErro
+        })
+    )
+})
 
 // A API esculta uma porta por onde recebe as requisições
 app.listen(config.get('api.porta'), () => console.log('A API está funcionando'));
--- SEMANA_10/NodeJS - Crie uma API REST padronizada e escalável/api-petshop/api/rotas/fornecedores/index.js
+++ SEMANA_10/NodeJS - Crie uma API REST padronizada e escalável/api-petshop/api/rotas/fornecedores/index.js
@@ -11,7 +11,7 @@ roteador.get('/', async (requisicao, resposta) => {
     )
 })
 
-roteador.post('/', async (requisicao, resposta) => {
+roteador.post('/', async (requisicao, resposta, proximo) => {
     try {
         const dadosRecebidos = requisicao.body
         const fornecedor = new Fornecedor(dadosRecebidos)
@@ -21,16 +21,11 @@ roteador.post('/', async (requisicao, resposta) => {
             JSON.stringify(fornecedor)
         )
     } catch (erro) {
-        resposta.status(400)
-        resposta.send(
-            JSON.stringify({
-                mensagem: erro.message
-            })
-        )
+        proximo(erro)
     }
 })
 
-roteador.get('/:idFornecedor', async (requisicao, resposta) => {
+roteador.get('/:idFornecedor', async (requisicao, resposta, proximo) => {
     try {
         const id = requisicao.params.idFornecedor
         const fornecedor = new Fornecedor({ id: id })
@@ -40,16 +35,11 @@ roteador.get('/:idFornecedor', async (requisicao, resposta) => {
             JSON.stringify(fornecedor)
         )
     } catch (erro) {
-        resposta.status(404)
-        resposta.send(
-            JSON.stringify({
-                mensagem: erro.message
-            })
-        )
+        proximo(erro)
     }
 })
 
-roteador.put('/:idFornecedor', async (requisicao, resposta) => {
+roteador.put('/:idFornecedor', async (requisicao, resposta, proximo) => {
     try {
         const id = requisicao.params.idFornecedor
         const dadosRecebidos = requisicao.body
@@ -59,16 +49,11 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => {
         resposta.status(204)
         resposta.end()
     } catch (erro) {
-        resposta.status(400)
-        resposta.send(
-            JSON.stringify({
-                mensagem: erro.message
-            })
-        )
+        proximo(erro)
     }
 })
 
-roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
+roteador.delete('/:idFornecedor', async (requisicao, resposta, proximo) => {
     try {
         const id = requisicao.params.idFornecedor
         const fornecedor = new Fornecedor({ id: id })
@@ -77,12 +62,7 @@ roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
         resposta.status(204)
         resposta.end()
     } catch (erro) {
-        resposta.status(404)
-        resposta.send(
-            JSON.stringify({
-                mensagem: erro.message
-            })
-        )
+        proximo(erro)
     }
 })
 
