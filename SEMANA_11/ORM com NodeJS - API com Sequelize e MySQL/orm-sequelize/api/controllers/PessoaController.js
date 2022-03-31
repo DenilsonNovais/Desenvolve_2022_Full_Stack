527b5389dadc2cf6d3b81807d6e8781ea21dbf89
Denilson Santos <denilson_n.santos@hotmail.com>
2022-03-31 19:48:29 -0300

    Implementando rotas para realizar  o CRUD

--- SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/controllers/PessoaController.js
+++ SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/controllers/PessoaController.js
@@ -12,7 +12,59 @@ class PessoaController {
         }catch(error) {
             return res.status(500).json(error.message);
         }
-    } 
+    }
+    
+    static async pegaUmaPessoa (req, res) {
+        const { id } = req.params;
+        try {
+            const umaPessoa = await database.Pessoas.findOne( { where: {
+                    id: Number(id)
+                }
+            });
+            return res.status(200).json(umaPessoa);
+        }catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async criarPessoa (req, res) {
+        const novaPessoa = req.body;
+        try {                       // Metodo que cria nova pessoa
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada);
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        } 
+    }
+
+    static async atualizaPessoa (req, res) {
+        const { id } = req.params;
+        const novaInfos = req.body;
+        try {
+            await database.Pessoas.update(novaInfos, {where: {id: Number(id)}});
+            const pessoaAtualizada = await database.Pessoas.findOne({ 
+                where: { 
+                    id: Number(id) 
+                }
+            })
+            return res.status(200).json(pessoaAtualizada)
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaPessoa (req, res) {
+        const { id } = req.params;
+        try {                      
+            await database.Pessoas.destroy({where: { id: Number(id) }});
+            return res.status(200).json({ mensagem: `id ${id} deletado`});
+
+        } catch (error) {
+            return res.status(500).json(error.message);
+        } 
+    }
 }
 
 module.exports = PessoaController;
--- SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/routes/pessoasRoute.js
+++ SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/routes/pessoasRoute.js
@@ -5,7 +5,19 @@ const PessoaController = require('../controllers/PessoaController');
 // Iniciando router
 const router = Router();
 
-// Chama todas as pessoas
+// Verbo Get - Pegar/Consultar
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+
+// Verbo Post - Criar/Adicionar
+router.post('/pessoas', PessoaController.criarPessoa);
+
+// Verbo Put - Atualizar/Alterar
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+
+// Verbo Delete - Deletar/Excluir
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
+
+
 
 module.exports = router;
