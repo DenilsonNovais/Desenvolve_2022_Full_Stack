0072178074d685ba08b73f1f236910173e652d32
Denilson Santos <denilson_n.santos@hotmail.com>
2022-04-01 01:34:57 -0300

    Referenciando, migrando e populando tabelas associadas

--- SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/models/pessoas.js
+++ SEMANA_11/ORM com NodeJS - API com Sequelize e MySQL/orm-sequelize/api/models/pessoas.js
@@ -1,26 +1,19 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pessoas extends Model {
-    /**
-       * Método auxiliar para definir associações.
-       * Este método não faz parte do ciclo de vida do Sequelize.
-       * O arquivo `models/index` chamará este método automaticamente.
-    */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Pessoas.init({
+  const Pessoas = sequelize.define('Pessoas', {
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
     email: DataTypes.STRING,
     role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoas',
-  });
+  }, {});
+  Pessoas.associate = function(models) {
+    Pessoas.hasMany(models.Turmas, {
+      foreignKey: 'docente_id'
+    }) 
+    Pessoas.hasMany(models.Matriculas, {
+      foreignKey: 'estudante_id'
+    })
+
+  };
   return Pessoas;
 };
