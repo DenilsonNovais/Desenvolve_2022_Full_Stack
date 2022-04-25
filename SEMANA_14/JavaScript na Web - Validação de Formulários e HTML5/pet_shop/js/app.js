18ac38cfbb4e8e6d0779d69fbb65dc4af3e36c39
Denilson Santos <denilson_n.santos@hotmail.com>
2022-04-25 16:09:26 -0300

    Adicionando validação de preço utilizando mascara

--- SEMANA_14/JavaScript na Web - Validação de Formulários e HTML5/pet_shop/js/app.js
+++ SEMANA_14/JavaScript na Web - Validação de Formulários e HTML5/pet_shop/js/app.js
@@ -3,6 +3,17 @@ import { valida } from './validacao.js'
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco') {
+        SimpleMaskMoney.setMask(input, {
+            prefix: 'R$ ', 
+            fixed: true,
+            fraçãoDigits : 2 , 
+            decimalSeparator : ',' , 
+            milharesSeparator : '.' , 
+            cursor : 'end' 
+        })
+    }
+
     input.addEventListener('blur', (evento) => {
         valida(evento.target)
     })
--- SEMANA_14/JavaScript na Web - Validação de Formulários e HTML5/pet_shop/js/validacao.js
+++ SEMANA_14/JavaScript na Web - Validação de Formulários e HTML5/pet_shop/js/validacao.js
@@ -55,6 +55,9 @@ const mensagensDeErro = {
     },
     estado: {
         valueMissing: 'O campo de estado não pode estar vazio.',
+    },
+    preco: {
+        valueMissing: 'O campo de preço não pode estar vazio.'
     }
 }
 
