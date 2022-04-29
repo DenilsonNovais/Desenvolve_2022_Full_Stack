b260202232a262abdcc1821268314d41a00b268b
Denilson Santos <denilson_n.santos@hotmail.com>
2022-04-28 22:01:44 -0300

    Iniciando curso J.Script O.Objetos com um index

---
+++ SEMANA_15/JavaScript - Programando a Orientação a Objetos/Byte_Bank/index.js
@@ -0,0 +1,9 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 88877766602;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
