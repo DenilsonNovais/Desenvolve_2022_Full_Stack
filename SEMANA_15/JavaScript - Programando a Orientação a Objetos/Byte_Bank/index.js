65d070ec8a381db42e4c34428d3b2a24b7c3da04
Denilson Santos <denilson_n.santos@hotmail.com>
2022-05-05 01:08:02 -0300

    Criando classes e usando encapsulamento

--- SEMANA_15/JavaScript - Programando a Orientação a Objetos/Byte_Bank/index.js
+++ SEMANA_15/JavaScript - Programando a Orientação a Objetos/Byte_Bank/index.js
@@ -1,9 +1,46 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente{
+    nome;
+    cpf;
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 88877766602;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+class ContaCorrente{
+    agencia;
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
